c01ede49095787032b41e581c959e13e39ed6763
Camila Moreira <camila.moreira1@dtidigital.com.br>
2025-12-23 17:03:04 -0300

    feat: config dagger hilt dependency injection

--- app/src/main/java/com/example/githubrepoapp/MainActivity.kt
+++ app/src/main/java/com/example/githubrepoapp/MainActivity.kt
@@ -10,7 +10,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.githubrepoapp.presentation.navigation.MainNavHost
 import com.example.githubrepoapp.ui.theme.GithubRepoAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/example/githubrepoapp/domain/usecase/GetRepoItemUseCase.kt
+++ app/src/main/java/com/example/githubrepoapp/domain/usecase/GetRepoItemUseCase.kt
@@ -2,8 +2,9 @@ package com.example.githubrepoapp.domain.usecase
 
 import com.example.githubrepoapp.data.remote.model.RepoItemResponse
 import com.example.githubrepoapp.domain.remote.repository.GithubRemoteRepository
+import javax.inject.Inject
 
-class GetRepoItemUseCase(
+class GetRepoItemUseCase @Inject constructor(
     private val repository: GithubRemoteRepository
 ) {
     suspend operator fun invoke(ownerName: String, repoName: String): RepoItemResponse {
