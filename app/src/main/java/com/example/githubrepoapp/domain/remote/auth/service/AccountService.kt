6f9ff598247c559bb52e291b924e3c5ce3ec6b21
Camila Moreira <camila.moreira1@dtidigital.com.br>
2025-12-30 13:39:15 -0300

    feat: add AccountService Interface

---
+++ app/src/main/java/com/example/githubrepoapp/domain/remote/auth/service/AccountService.kt
@@ -0,0 +1,21 @@
+package com.example.githubrepoapp.domain.remote.auth.service
+
+import com.example.githubrepoapp.domain.remote.auth.model.User
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+
+interface AccountService {
+    val currentUser: Flow<User?>
+        get() = callbackFlow {
+
+        }
+
+    val currentUserId: String
+
+    fun hasUser(): Boolean
+
+    suspend fun logIn(email: String, password: String)
+    suspend fun signUp(email: String, password: String)
+    suspend fun logOut()
+    suspend fun deleteAccount()
+}
